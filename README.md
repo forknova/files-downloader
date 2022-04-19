6b796b98e67d828594c70e74b1911bbfa5188a14
forknova <softechstar.jsh@outlook.com>
2022-04-19 14:06:35 +0200

    v1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# shopify-file-downloader
+# Shopify file downloader
+
+This allows you to download all assets in shopify's /admin/files.
+
+### Configure
+
+1. Create a new Shopify custom app.
+2. Under Configuration select "Admin API integration" 
+   1. Only access scope needed is `read_files` [see here](screenshot.png)
+3. Install app
+4. Fill out [config.json](config.json)
+5. run `yarn start` or `npm run start` in console
---
+++ index.js
@@ -0,0 +1,124 @@
+import fs from 'fs';
+import fetch from 'node-fetch';
+import data from './config.json'
+
+const store = data.store;
+const accessToken = data.accessToken;
+const apiVersion = '2022-04';
+const apiLocation = '/admin/api/';
+
+const shopGraphQl = 'https://' + store + apiLocation + apiVersion + '/graphql.json';
+
+let files = [];
+
+function downloadFiles (file_urls) {
+  if (!fs.existsSync("./files")) fs.mkdirSync("./files");
+  if (!fs.existsSync("./files/1")) fs.mkdirSync("./files/1");
+
+  let dir = "1";
+  let count = 0;
+
+  const promises = file_urls.map((file_url, idx) => {
+    return fetch(file_url)
+      .then((res) => {
+        if (count % 200 === 0 && count !== 0) {
+          dir = (+dir + 1).toString();
+          if (!fs.existsSync("./files/" + dir)) fs.mkdirSync("./files/" + dir);
+        }
+
+        var file_name = res.url.split("/").pop().split("?")[0];
+        var dest = fs.createWriteStream(`./files/${dir}/${file_name}`);
+        res.body.pipe(dest);
+        count++;
+        process.stdout.write(`Downloading file ${count} of ${file_urls.length}\r`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  Promise.all(promises)
+    .then(() => {
+      process.stdout.write("Success. All files downloaded!\r");
+    })
+    .catch((err) => {
+      console.log("Error!")
+    });
+}
+
+function getFiles (cursor = null) {
+  return fetch(
+    shopGraphQl,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Access-Token": accessToken
+      },
+      body: JSON.stringify({
+        query: `{
+  files(first:50${cursor ? ',after:"'+ cursor +'"' : ''}) {
+    edges {
+      cursor
+      node {
+        ... on GenericFile {
+          url
+        }
+        ... on MediaImage {
+          image {
+            originalSrc: url
+          }
+        }
+        ...VideoFragment
+        fileErrors {
+          code
+          details
+          message
+        }
+      }
+    }
+    pageInfo {
+      hasNextPage
+    }
+  }
+}
+
+fragment VideoFragment on Video {
+  originalSource {
+    url
+  }
+}`})
+    }
+  )
+    .then(res => {
+      return res.json();
+    })
+    .then((result) => {
+      if(result.data.files.edges.length) {
+        result.data.files.edges.forEach(({node}) => {
+          let file
+          if (node.url) {
+            file = node.url
+          } else if (node.image.originalSrc) {
+            file = node.image.originalSrc
+          } else if (node.originalSource.url) {
+            file = node.originalSource.url
+          }
+
+          files.push(file)
+        });
+
+         if (result.data.files.pageInfo.hasNextPage) {
+           getFiles(result.data.files.edges[result.data.files.edges.length - 1].cursor)
+         } else {
+           process.stdout.write("Files fetched\r");
+           downloadFiles(files)
+         }
+      } else {
+
+      }
+    })
+    .catch(err => console.error(err));
+}
+
+getFiles()
